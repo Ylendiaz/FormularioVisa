a4b7fdcced2f78b4d7d24e49b3240e2c8cd8a7c4
Ylen Diaz <ylendiazpolanco@gmail.com>
2022-08-23 01:49:51 -0400

    first commit

---
+++ FormularioVisa/Recibe.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace FormularioVisa
+{
+    public partial class Recibe : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Nombre: "+ Request["first-name"] + "<br>" + "Apellido: " + Request["last-name"] + "<br>" + "Sexo: " + Request["sex"] + "<br>" + "Fecha de Nacimiento: " + Request["birth-date"] + "<br>" + "Dirección: " + Request["user-address"] + "<br>" + "Sueldo: " + Request["user-salary"] + "<br>" + "Empleador: " + Request["user-employer"] + "<br>" + "Fecha de Ingreso: " + Request["admission-date"] + "<br>" + "Cantidad de Hijos: " + Request["number-children"] + "<br>" + "Cantidad de Padres: " + Request["number-parents"]);
+        }
+    }
+}
